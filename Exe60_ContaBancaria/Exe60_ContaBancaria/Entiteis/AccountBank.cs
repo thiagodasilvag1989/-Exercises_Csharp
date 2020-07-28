23f12bf3ee062a7555844f72d6fc0e29c091eccb
Thiago da Silva <thiagodasilvag1989@gmail.com>
2020-07-28 20:22:28 -0300

    Exercicio simulando uma conta bancaria

---
+++ Exe60_ContaBancaria/Exe60_ContaBancaria/Entiteis/AccountBank.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exe60_ContaBancaria.Entiteis
+{
+    class AccountBank
+    {
+        //Atributos da Conta
+        public int NumeroConta { get; private set; }
+        public string NomeTitular { get; private set; }
+        public double Saldo { get; private set; }
+
+        //Construtor
+        public AccountBank(int numeroConta, string nomeTitular, double saldo)
+        {
+            NumeroConta = numeroConta;
+            NomeTitular = nomeTitular;
+            Saldo = saldo;
+        }
+        public AccountBank()
+        {
+
+        }
+
+        public void DepositoInicial (char cond)
+        {
+            if (cond == 's' || cond == 'S')
+            {
+                Console.Write("Entre com Valor de Deposito Inicial: ");
+                Saldo = (double.Parse(Console.ReadLine()));
+
+            }
+        }
+
+        public void AddNumConta(int numeroConta)
+        {
+            NumeroConta = numeroConta;
+        }
+
+        public void AddTitular (string nomeTitular)
+        {
+            NomeTitular = nomeTitular;
+        }
+
+        public void Deposito(double saldo)
+        {
+            Saldo += saldo;
+        }
+        public void Saque(double saldo)
+        {
+            Saldo -= saldo+5;
+        }
+
+        public override string ToString()
+        {
+            return "Conta: " + NumeroConta + ", Titular da Conta: " + NomeTitular + ", Saldo R$: " + Saldo;
+        }
+    }
+}
---
+++ Exe60_ContaBancaria/Exe60_ContaBancaria/Program.cs
@@ -0,0 +1,39 @@
+﻿using Exe60_ContaBancaria.Entiteis;
+using System;
+
+
+namespace Exe60_ContaBancaria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instancia d Classe AccountBank
+            AccountBank account = new AccountBank();
+            //Entarda de dados
+            Console.Write("Entre o Numero da Conta: ");
+            account.AddNumConta(int.Parse(Console.ReadLine()));
+            Console.Write("Entre o Titular da Conta: ");
+            account.AddTitular(Console.ReadLine());
+            Console.Write("Haverá deposito inicial? (s/n): ");
+            account.DepositoInicial(char.Parse(Console.ReadLine()));
+            
+            Console.WriteLine();
+            Console.WriteLine(account.ToString());
+
+            Console.Write("Entre um valor de deposito: ");
+            account.Deposito(double.Parse(Console.ReadLine()));
+            Console.WriteLine();
+
+            Console.WriteLine("Dados da Conta Atualizados:");
+            Console.WriteLine(account.ToString());
+            Console.WriteLine();
+
+            Console.Write("Entre um valor para saque: ");
+            account.Saque(double.Parse(Console.ReadLine()));
+            Console.WriteLine();
+            Console.WriteLine("Dados da Conta Atualizados:");
+            Console.WriteLine(account.ToString());
+        }
+    }
+}
